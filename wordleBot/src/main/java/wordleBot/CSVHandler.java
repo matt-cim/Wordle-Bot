45d3ddf2b454bb6a2c734b0786fe37924e275aa4
matt-cim <mcimmy91@gmail.com>
2022-07-17 18:14:00 -0400

    CSV class and player hash

---
+++ wordleBot/src/main/java/wordleBot/CSVHandler.java
@@ -0,0 +1,157 @@
+package wordleBot;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
+public class CSVHandler {
+	
+	private String name;
+	private String[] stats;
+
+	public CSVHandler(String name, String[] stats) {
+		this.name = name;
+		this.stats = stats;
+	}
+	
+	
+	
+	 public void writeToCSV () {
+		 
+		 String[] info1 = {this.name, this.stats[0], 
+				 			this.stats[1], this.stats[2], 
+				 			this.stats[3], this.stats[4]
+				 			};
+		 
+	     List<String[]> entries = new ArrayList<>();
+	     entries.add(info1);
+
+	     
+//	     https://www.geeksforgeeks.org/writing-a-csv-file-in-java-using-opencsv/
+	     File file = new File("src/main/java/infoCatalog.csv");
+	     
+	     try {
+	         // create FileWriter object with file as parameter
+	         FileWriter outputfile = new FileWriter(file);
+	   
+	         // create CSVWriter object filewriter object as parameter
+	         CSVWriter writer = new CSVWriter(outputfile);
+	         
+	         
+	         writer.writeAll(entries);
+	   
+//	         // adding header to csv
+//	         String[] header = { "Name", "Class", "Marks" };
+//	         writer.writeNext(header);
+//	   
+//	         // add data to csv
+//	         String[] data1 = { "Aman", "10", "620" };
+//	         writer.writeNext(data1);
+//	         String[] data2 = { "Suraj", "10", "630" };
+//	         writer.writeNext(data2);
+	   
+	         // closing writer connection
+	         writer.close();
+	     }
+	     catch (IOException e) {
+	         // TODO Auto-generated catch block
+	         e.printStackTrace();
+	     }
+	     
+	     
+	 }
+	 
+	 public void readCSV (){
+		   
+	     try {
+	   
+	         // Create an object of filereader
+	         // class with CSV file as a parameter.
+	         FileReader filereader = new FileReader("src/main/java/infoCatalog.csv");
+	   
+	         try (// create csvReader object passing
+				         // file reader as a parameter
+			CSVReader csvReader = new CSVReader(filereader)) {
+				String[] nextRecord;
+   
+				 // we are going to read data line by line
+				 while ((nextRecord = csvReader.readNext()) != null) {
+				     for (String cell : nextRecord) {
+				         System.out.print(cell + "\t");
+				     }
+				     System.out.println();
+				 }
+				 filereader.close();
+			}
+	     }
+	     catch (Exception e) {
+	         e.printStackTrace();
+	     }
+	     
+	 }
+	 
+	 
+	 public void updateCSV (){
+		   
+	     try {
+	   
+	         // Create an object of filereader
+	         // class with CSV file as a parameter.
+	         FileReader filereader = new FileReader("src/main/java/infoCatalog.csv");
+	   
+	         try (// create csvReader object passing
+				         // file reader as a parameter
+			CSVReader csvReader = new CSVReader(filereader)) {
+	        	 
+	        	 List<String[]> csvBody = csvReader.readAll();
+	        	 csvBody.get(0)[1] = "magic";
+	        	 filereader.close();
+	        	 
+	        	 
+	        	 
+	    	     File file = new File("src/main/java/infoCatalog.csv");
+	    	     
+	    	     try {
+	    	         // create FileWriter object with file as parameter
+	    	         FileWriter outputfile = new FileWriter(file);
+	    	   
+	    	         // create CSVWriter object filewriter object as parameter
+	    	         CSVWriter writer = new CSVWriter(outputfile);
+	    	         
+	    	         
+	    	         writer.writeAll(csvBody);
+	    	   
+//	    	         // adding header to csv
+//	    	         String[] header = { "Name", "Class", "Marks" };
+//	    	         writer.writeNext(header);
+//	    	   
+//	    	         // add data to csv
+//	    	         String[] data1 = { "Aman", "10", "620" };
+//	    	         writer.writeNext(data1);
+//	    	         String[] data2 = { "Suraj", "10", "630" };
+//	    	         writer.writeNext(data2);
+	    	   
+	    	         // closing writer connection
+	    	         writer.close();
+	    	     }
+	    	     catch (IOException e) {
+	    	         // TODO Auto-generated catch block
+	    	         e.printStackTrace();
+	    	     }
+	        	 
+			}
+	     }
+	     catch (Exception e) {
+	         e.printStackTrace();
+	     }
+	 }
+	 
+	 
+	 
+}
--- wordleBot/src/main/java/wordleBot/Worden.java
+++ wordleBot/src/main/java/wordleBot/Worden.java
@@ -14,6 +14,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -32,12 +33,17 @@ import com.opencsv.CSVWriter;
 //	actual javadoc for JDA
 
 // hash map- key is userID value is all stats boom
+
+// games played, win percentage, current streak, max streak, guess distribution-> ones present on wordle
 //**************************************************
 
 
 
 public class Worden extends ListenerAdapter {
+	
+	private HashMap<String, String[]> playerInfo = new HashMap<String, String[]>();
 
+	
 	public static void main (String[] args) {
 		
 //		using create default because otherwise would have to specify intent ex "listening"
@@ -58,161 +64,56 @@ public class Worden extends ListenerAdapter {
 		 MessageChannel channel = event.getChannel();
 		 Pattern regex = Pattern.compile(".*let-the-games-begin.*");
 		 Matcher correctChannel = regex.matcher(channel.getName());
+		 
 	
 		 if (!event.getAuthor().isBot() && correctChannel.matches()) {
 //			 getMessage() has other misc info, but must extract raw content
 		        String text = event.getMessage().getContentRaw(); 
-		        System.out.println("content   " + text);
+//		        System.out.println("content   " + text);
 	            channel.sendMessage("I have recieved your Wordle").queue();
 	            
-	            writeToCSV(event.getAuthor().toString());
-	            readCSV();
+	            String playerName = event.getAuthor().toString();
 	            
 	            
-	            if (text.equals("update")) {
-	            	updateCSV();
+	   		 	Pattern wordleRegex = Pattern.compile("Wordle \\d+ (1|2|3|4|5|6)\\/6.");
+	   		 	Matcher correctWordle = wordleRegex.matcher(text);
+	    
+	            
+	            if (!playerExists(playerName) && !correctWordle.matches()) {
+	            	
+	            	String[] defaultStats = {"0", "0", "0", "0", "0"};
+	            	playerInfo.put(playerName, defaultStats);
+	            	
+	            	CSVHandler csvHandler = new CSVHandler(playerName, defaultStats);
+	            	
+		            csvHandler.writeToCSV();
+		            
+		            csvHandler.readCSV();
+		                 
+		            if (text.equals("update")) {
+		            	csvHandler.updateCSV();
+		            }
+	            	
 	            }
+//	            else if (playerExists(playerName) && correctWordle.matches()) {
+//	            	System.out.println(playerInfo.get(playerName));
+//	            }
+	            
+	            
+	            System.out.println(correctWordle.matches());
+	            
+	            
 		 }
 	        
 	 }
 	 
 	 
-	 public static void writeToCSV (String name) {
-		 
-		 String[] info1 = {name, "change me"};
-		 
-	     List<String[]> entries = new ArrayList<>();
-	     entries.add(info1);
-
-	     
-//	     https://www.geeksforgeeks.org/writing-a-csv-file-in-java-using-opencsv/
-	     File file = new File("src/main/java/infoCatalog.csv");
-	     
-	     try {
-	         // create FileWriter object with file as parameter
-	         FileWriter outputfile = new FileWriter(file);
-	   
-	         // create CSVWriter object filewriter object as parameter
-	         CSVWriter writer = new CSVWriter(outputfile);
-	         
-	         
-	         writer.writeAll(entries);
-	   
-//	         // adding header to csv
-//	         String[] header = { "Name", "Class", "Marks" };
-//	         writer.writeNext(header);
-//	   
-//	         // add data to csv
-//	         String[] data1 = { "Aman", "10", "620" };
-//	         writer.writeNext(data1);
-//	         String[] data2 = { "Suraj", "10", "630" };
-//	         writer.writeNext(data2);
-	   
-	         // closing writer connection
-	         writer.close();
-	     }
-	     catch (IOException e) {
-	         // TODO Auto-generated catch block
-	         e.printStackTrace();
-	     }
-	     
-	     
-	 }
+	 private boolean playerExists (String name) { return playerInfo.containsKey(name); }
 	 
-	 
-	 
-	 public static void readCSV (){
-	   
-	     try {
-	   
-	         // Create an object of filereader
-	         // class with CSV file as a parameter.
-	         FileReader filereader = new FileReader("src/main/java/infoCatalog.csv");
-	   
-	         try (// create csvReader object passing
-				         // file reader as a parameter
-			CSVReader csvReader = new CSVReader(filereader)) {
-				String[] nextRecord;
-   
-				 // we are going to read data line by line
-				 while ((nextRecord = csvReader.readNext()) != null) {
-				     for (String cell : nextRecord) {
-				         System.out.print(cell + "\t");
-				     }
-				     System.out.println();
-				 }
-				 filereader.close();
-			}
-	     }
-	     catch (Exception e) {
-	         e.printStackTrace();
-	     }
-	     
-	 }
-	 
-	 
-	 
-	 public static void updateCSV (){
-		   
-	     try {
-	   
-	         // Create an object of filereader
-	         // class with CSV file as a parameter.
-	         FileReader filereader = new FileReader("src/main/java/infoCatalog.csv");
-	   
-	         try (// create csvReader object passing
-				         // file reader as a parameter
-			CSVReader csvReader = new CSVReader(filereader)) {
-	        	 
-	        	 List<String[]> csvBody = csvReader.readAll();
-	        	 csvBody.get(0)[1] = "magic";
-	        	 filereader.close();
-	        	 
-	        	 
-	        	 
-	    	     File file = new File("src/main/java/infoCatalog.csv");
-	    	     
-	    	     try {
-	    	         // create FileWriter object with file as parameter
-	    	         FileWriter outputfile = new FileWriter(file);
-	    	   
-	    	         // create CSVWriter object filewriter object as parameter
-	    	         CSVWriter writer = new CSVWriter(outputfile);
-	    	         
-	    	         
-	    	         writer.writeAll(csvBody);
-	    	   
-//	    	         // adding header to csv
-//	    	         String[] header = { "Name", "Class", "Marks" };
-//	    	         writer.writeNext(header);
-//	    	   
-//	    	         // add data to csv
-//	    	         String[] data1 = { "Aman", "10", "620" };
-//	    	         writer.writeNext(data1);
-//	    	         String[] data2 = { "Suraj", "10", "630" };
-//	    	         writer.writeNext(data2);
-	    	   
-	    	         // closing writer connection
-	    	         writer.close();
-	    	     }
-	    	     catch (IOException e) {
-	    	         // TODO Auto-generated catch block
-	    	         e.printStackTrace();
-	    	     }
-	        	 
-			}
-	     }
-	     catch (Exception e) {
-	         e.printStackTrace();
-	     }
-	 }
-	 
+	 private void clearHash () { playerInfo.clear(); }
 	 
 
 	 
-	 
-
-
 //TODO 1. create regex that matches wordle score 2. auto assign permissions on join 3. extract stats from  text in private channel
 //	 lowkey could just constantly write stats to text file and if server goes down call function that sets hash
 //	back up
