b26efa39debbb35e739ff902491a961c3572ebfc
matt-cim <mcimmy91@gmail.com>
2022-07-22 23:18:32 -0400

    fixed jar file search issue

--- wordleBot/src/main/java/wordleBot/Worden.java
+++ wordleBot/src/main/java/wordleBot/Worden.java
@@ -1,28 +1,33 @@
 package wordleBot;
 
-import net.dv8tion.jda.api.JDA;
+//import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.entities.ChannelType;
-import net.dv8tion.jda.api.entities.Message;
+//import net.dv8tion.jda.api.entities.Activity;
+//import net.dv8tion.jda.api.entities.ChannelType;
+//import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import java.io.File;
+import java.io.BufferedReader;
+//import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+//import java.io.FileWriter;
+//import java.io.IOException;
+//import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
+//import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 //https://zetcode.com/java/opencsv/
 import com.opencsv.CSVReader;
-import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvValidationException;
+//import com.opencsv.CSVWriter;
 
 //**************************************************
 //	https://github.com/DV8FromTheWorld/JDA/wiki - wiki section for examples
@@ -53,12 +58,31 @@ public class Worden extends ListenerAdapter {
 			e.printStackTrace();
 		}
 		
-		System.out.println("Worden bot is up and running!");
+		CSVHandler csvHandler = new CSVHandler();
 		
-		readCSVAndUpdateHash();
+		try {
+			playerInfo = csvHandler.readCSVAndUpdateHash();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+		
+		System.out.println("Worden bot is up and running!");
 		
 		printHash();
 	}
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	private static void printHash () {
@@ -72,98 +96,56 @@ public class Worden extends ListenerAdapter {
 	}
 	
 	
-	// imperative on bot start as server crash would reset hash
-	public static void readCSVAndUpdateHash () { 
-		 	
-     	int i, j; 
-		String[] stats = new String[5];
-     	String playerName;
 
-     	
-	     try {
-	   
-	    	 FileReader filereader = new FileReader("src/main/java/infoCatalog.csv");
-	   
-	         try (CSVReader csvReader = new CSVReader(filereader)) {
-	        	 // read each line of CSV and parse so that user name and statistics are separate
-	        	 String[] nextLine;
-   
-				 while ((nextLine = csvReader.readNext()) != null) {
-					 playerName = "";
-					 i = 0;
-					 j = 0;
-					 
-				     for (String cell : nextLine) {
-				    	 
-				    	 if (j > 0) {
-				    		 stats[i] = cell;
-				    		 i ++;
-				    	 }
-				    	 else {
-				    		 playerName = cell;
-				    	 }
-				    	 j ++;
-				     }
-				     // final step, key and value have been initialized
-				     playerInfo.put(playerName, stats);  
-				 }
-				 filereader.close();
-			}
-	     }
-	     catch (Exception e) {
-	         e.printStackTrace();
-	     }
-	     
-	 }
 	
 	
-	 //	recognizes a message has been set in the appropriate channel and responds accordingly
-	@Override
-	public void onMessageReceived (MessageReceivedEvent event) {
-		 
-		MessageChannel channel = event.getChannel();
-		Pattern regex = Pattern.compile(".*let-the-games-begin.*");
-		Matcher correctChannel = regex.matcher(channel.getName());
-		 
-	
-		if (!event.getAuthor().isBot() && correctChannel.matches()) {
-			 
-			// getMessage() has other misc info, but must extract raw content
-			String text = event.getMessage().getContentRaw(); 
-	        channel.sendMessage("I have recieved your Wordle").queue();
-	            
-	        String playerName = event.getAuthor().toString();
-	            
-	            
-	   		Pattern wordleRegex = Pattern.compile("[W|w]ordle (\\d+) (1|2|3|4|5|6)\\/6\\*?(\\n.*){2,}");
-	   		Matcher correctWordle = wordleRegex.matcher(text);
-	   		 
-	   		String[] defaultStats = {"0", "0", "0", "0", "0"};
-	   		playerInfo.put(playerName, defaultStats);
-            	
-	   		CSVHandler csvHandler = new CSVHandler(playerName, defaultStats);
-            	
-	   		csvHandler.writeToCSV();
-            
-		            
-//		            if (text.equals("update")) {
-//		            	csvHandler.updateCSV("","","","",correctWordle.group(1));
-//		            }
-	   		 
-	   		if (playerExists(playerName) && correctWordle.matches()) {
-	   			System.out.println(correctWordle.group(1));
-	   			csvHandler.updateCSV("","","","",correctWordle.group(2));
-	   		}
-	            	            
-	   		System.out.println(correctWordle.matches());     
-		 }
-	        
-	 }
+//	 //	recognizes a message has been set in the appropriate channel and responds accordingly
+//	@Override
+//	public void onMessageReceived (MessageReceivedEvent event) {
+//		 
+//		MessageChannel channel = event.getChannel();
+//		Pattern regex = Pattern.compile(".*let-the-games-begin.*");
+//		Matcher correctChannel = regex.matcher(channel.getName());
+//		 
+//	
+//		if (!event.getAuthor().isBot() && correctChannel.matches()) {
+//			 
+//			// getMessage() has other misc info, but must extract raw content
+//			String text = event.getMessage().getContentRaw(); 
+//	        channel.sendMessage("I have recieved your Wordle").queue();
+//	            
+//	        String playerName = event.getAuthor().toString();
+//	            
+//	            
+//	   		Pattern wordleRegex = Pattern.compile("[W|w]ordle (\\d+) (1|2|3|4|5|6)\\/6\\*?(\\n.*){2,}");
+//	   		Matcher correctWordle = wordleRegex.matcher(text);
+//	   		 
+//	   		String[] defaultStats = {"0", "0", "0", "0", "0"};
+//	   		playerInfo.put(playerName, defaultStats);
+//            	
+//	   		CSVHandler csvHandler = new CSVHandler(playerName, defaultStats);
+//            	
+//	   		csvHandler.writeToCSV();
+//            
+//		            
+////		            if (text.equals("update")) {
+////		            	csvHandler.updateCSV("","","","",correctWordle.group(1));
+////		            }
+//	   		 
+//	   		if (playerExists(playerName) && correctWordle.matches()) {
+//	   			System.out.println(correctWordle.group(1));
+//	   			csvHandler.updateCSV("","","","",correctWordle.group(2));
+//	   		}
+//	            	            
+//	   		System.out.println(correctWordle.matches());     
+//		 }
+//	        
+//	 }
+//	 
+//	 
+//	 private boolean playerExists (String playerName) { return playerInfo.containsKey(playerName); }
 	 
-	 
-	 private boolean playerExists (String playerName) { return playerInfo.containsKey(playerName); }
-	 
-	 private void clearHash () { playerInfo.clear(); }
+//	 private void clearHash () { playerInfo.clear(); }
 	 
 
 	 
